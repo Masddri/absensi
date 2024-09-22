4384caf2893bd450f0b6e0805248779ba44dafba
Masdrii <adribektyawan@gmail.com>
2024-09-22 09:34:58 +0700

    commit awal

---
+++ app/Models/Cuti.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cuti extends Model
+{
+    use HasFactory;
+
+    public $fillable = ['alasan', 'jumlah_cuti', 'tanggal_cuti','deskripsi','id_karyawan'];
+    public $visible = ['nama_karyawan', 'jam_masuk', 'jam_keluar','id_karyawan'];
+
+    public function Absensi()
+    {
+        return $this->belongsTo(Absensi::class, 'id_karyawan');
+    }
+
+}
